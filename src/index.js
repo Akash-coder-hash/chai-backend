80bc57f1daeb9982586b97a4a970c95a59be8860
Akash <vv.akku111@gmail.com>
2024-07-23 20:58:40 +0530

    Mongo connection setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import dotenv from 'dotenv';
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
+
+
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
